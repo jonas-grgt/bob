94e4403fd4e4745f56dd64e3ebf47602d9f8c06b
Joran Van Belle <joran.vanbelle@live.be>
2024-05-06 11:42:20 +0200

    chore(processor): replace CI JRE condition by Junit conditions

--- processor/src/test/java/io/jonasg/bob/BobTests.java
+++ processor/src/test/java/io/jonasg/bob/BobTests.java
@@ -2,11 +2,14 @@ package io.jonasg.bob;
 
 import java.util.List;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import io.toolisticon.cute.Cute;
 import io.toolisticon.cute.CuteApi;
 import io.toolisticon.cute.JavaFileObjectUtils;
+import org.junit.jupiter.api.condition.EnabledForJreRange;
+import org.junit.jupiter.api.condition.JRE;
 
 public class BobTests {
 
@@ -239,5 +242,29 @@ public class BobTests {
 						JavaFileObjectUtils.readFromResource(
 								"/tests/AllPublicSettersThatHaveCorrespondingFieldsAreBuildable/Expected_AllPublicSettersThatHaveCorrespondingFieldsAreBuildable.java"))
 				.executeTest();
+	}
+
+	@Nested
+	@EnabledForJreRange(min = JRE.JAVA_12, disabledReason = "Records do not exist yet")
+	class RecordTests {
+
+		@Test
+		public void recordsAreBuildable() {
+			Cute.blackBoxTest()
+					.given()
+					.processors(List.of(BuildableProcessor.class))
+					.andSourceFiles("/tests/RecordsAreBuildable/RecordsAreBuildable.java")
+					.whenCompiled()
+					.thenExpectThat()
+					.compilationSucceeds()
+					.andThat()
+					.generatedSourceFile("io.jonasg.bob.test.RecordsAreBuildableBuilder")
+					.matches(
+							CuteApi.ExpectedFileObjectMatcherKind.BINARY,
+							JavaFileObjectUtils.readFromResource(
+									"/tests/RecordsAreBuildable/Expected_RecordsAreBuildable.java"))
+					.executeTest();
+		}
+
 	}
 }
